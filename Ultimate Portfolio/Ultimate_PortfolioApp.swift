96d908e2217313c42243f7bc2f28ef3ab5b1b9cc
Manuel Lorenzo <manulorenzop@gmail.com>
2021-11-16 07:46:30 +0100

    Initial Commit

---
+++ Ultimate Portfolio/Ultimate_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_PortfolioApp.swift
+//  Ultimate Portfolio
+//
+//  Created by Manu on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
