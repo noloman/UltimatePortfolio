b3af43252c63a1aff156e05984f826510f548dec
Manuel Lorenzo <manulorenzop@gmail.com>
2021-11-16 08:39:36 +0100

    create core data models and DataController

---
+++ Ultimate Portfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,25 @@
+//
+//  Ultimate_PortfolioApp.swift
+//  Ultimate Portfolio
+//
+//  Created by Manu on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_PortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
