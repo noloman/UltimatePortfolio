027996a6df64e107b63678c9bde97193dc780f90
Manuel Lorenzo <manulorenzop@gmail.com>
2021-11-22 21:15:55 +0100

    core data code cleanup

---
+++ Ultimate Portfolio/Item-CoreDataHelpers.swift
@@ -0,0 +1,34 @@
+//
+//  Item-CoreDataHelper.swift
+//  Ultimate Portfolio
+//
+//  Created by Manu on 22/11/2021.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+
+    var itemDetail: String {
+        detail ?? ""
+    }
+
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+    
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        return item
+    }
+}
